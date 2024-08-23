9f5d49fcc8c6d1c294777557f70169964c7a92c1
Shabinder Singh <contact@shabinder.in>
2024-08-23 22:04:33 +0530

    Dep Updates

--- src/androidMain/kotlin/in/shabinder/soundbound/utils/Context.kt
+++ src/androidMain/kotlin/in/shabinder/soundbound/utils/Context.kt
@@ -6,5 +6,8 @@ import androidx.compose.runtime.Immutable
 actual typealias Context = AppContextProvider
 
 @Immutable
-open class AppContextProvider(val value: Context, val activityContext: Context? = null)
+open class AppContextProvider(
+    val value: Context,
+    val activityContext: () -> Context? = { null }
+)
 // additional activity context, since we operate in single activity mode
