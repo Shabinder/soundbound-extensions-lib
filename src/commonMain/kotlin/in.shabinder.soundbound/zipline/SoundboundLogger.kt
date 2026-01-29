0db37f01de30291a74462f2f5642a4c2d4f05528
Shabinder Singh <shabinder.singh@hoichoi.tech>
2026-01-29 14:14:53 +0530

    Add test logger and integration tests for various providers, including Soundcloud, Jamendo, Gaana, and Youtube

--- src/commonMain/kotlin/in.shabinder.soundbound/zipline/SoundboundLogger.kt
+++ src/commonMain/kotlin/in.shabinder.soundbound/zipline/SoundboundLogger.kt
@@ -29,51 +29,63 @@ interface SoundboundLogger: ZiplineService  {
     )
 }
 
+/**
+ * Formats a message with exception details if a throwable is provided.
+ * Format: "message [ExceptionClass: exception message]"
+ */
+fun formatMessageWithThrowable(message: String, throwable: Throwable?): String {
+    return if (throwable != null) {
+        val exceptionInfo = "${throwable::class.simpleName ?: "Exception"}: ${throwable.message ?: "no message"}"
+        "$message [$exceptionInfo]"
+    } else {
+        message
+    }
+}
 
 fun SoundboundLogger.v(throwable: Throwable? = null, tag: String = this.tag, message: () -> String) {
-    log(Severity.Verbose, tag, throwable?.toThrowableWrapper(), message())
+    log(Severity.Verbose, tag, throwable?.toThrowableWrapper(), formatMessageWithThrowable(message(), throwable))
 }
 
 fun SoundboundLogger.d(throwable: Throwable? = null, tag: String = this.tag, message: () -> String) {
-    log(Severity.Debug, tag, throwable?.toThrowableWrapper(), message())
+    log(Severity.Debug, tag, throwable?.toThrowableWrapper(), formatMessageWithThrowable(message(), throwable))
 }
 
 fun SoundboundLogger.i(throwable: Throwable? = null, tag: String = this.tag, message: () -> String) {
-    log(Severity.Info, tag, throwable?.toThrowableWrapper(), message())
+    log(Severity.Info, tag, throwable?.toThrowableWrapper(), formatMessageWithThrowable(message(), throwable))
 }
 
 fun SoundboundLogger.w(throwable: Throwable? = null, tag: String = this.tag, message: () -> String) {
-    log(Severity.Warn, tag, throwable?.toThrowableWrapper(), message())
+    log(Severity.Warn, tag, throwable?.toThrowableWrapper(), formatMessageWithThrowable(message(), throwable))
 }
 
 fun SoundboundLogger.e(throwable: Throwable? = null, tag: String = this.tag, message: () -> String) {
-    log(Severity.Error, tag, throwable?.toThrowableWrapper(), message())
+    log(Severity.Error, tag, throwable?.toThrowableWrapper(), formatMessageWithThrowable(message(), throwable))
 }
 
 fun SoundboundLogger.a(throwable: Throwable? = null, tag: String = this.tag, message: () -> String) {
-    log(Severity.Assert, tag, throwable?.toThrowableWrapper(), message())
+    log(Severity.Assert, tag, throwable?.toThrowableWrapper(), formatMessageWithThrowable(message(), throwable))
 }
 
 inline fun SoundboundLogger.v(messageString: String, throwable: Throwable? = null, tag: String = this.tag) {
-    log(Severity.Verbose, tag, throwable?.toThrowableWrapper(), messageString)
+    log(Severity.Verbose, tag, throwable?.toThrowableWrapper(), formatMessageWithThrowable(messageString, throwable))
 }
 
 inline fun SoundboundLogger.d(messageString: String, throwable: Throwable? = null, tag: String = this.tag) {
-    log(Severity.Debug, tag, throwable?.toThrowableWrapper(), messageString)
+    log(Severity.Debug, tag, throwable?.toThrowableWrapper(), formatMessageWithThrowable(messageString, throwable))
 }
 
 inline fun SoundboundLogger.i(messageString: String, throwable: Throwable? = null, tag: String = this.tag) {
-    log(Severity.Info, tag, throwable?.toThrowableWrapper(), messageString)
+    log(Severity.Info, tag, throwable?.toThrowableWrapper(), formatMessageWithThrowable(messageString, throwable))
 }
 
 inline fun SoundboundLogger.w(messageString: String, throwable: Throwable? = null, tag: String = this.tag) {
-    log(Severity.Warn, tag, throwable?.toThrowableWrapper(), messageString)
+    log(Severity.Warn, tag, throwable?.toThrowableWrapper(), formatMessageWithThrowable(messageString, throwable))
 }
 
 inline fun SoundboundLogger.e(messageString: String, throwable: Throwable? = null, tag: String = this.tag) {
-    log(Severity.Error, tag, throwable?.toThrowableWrapper(), messageString)
+    log(Severity.Error, tag, throwable?.toThrowableWrapper(), formatMessageWithThrowable(messageString, throwable))
 }
 
 inline fun SoundboundLogger.a(messageString: String, throwable: Throwable? = null, tag: String = this.tag) {
-    log(Severity.Assert, tag, throwable?.toThrowableWrapper(), messageString)
+    log(Severity.Assert, tag, throwable?.toThrowableWrapper(), formatMessageWithThrowable(messageString, throwable))
 }
